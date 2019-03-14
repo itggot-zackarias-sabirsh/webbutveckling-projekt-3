110251ea14529d3e9265314064e871004e2acd95
itggot-zackarias-sabirsh <zackarias.sabirsh@elev.ga.ntig.se>
2019-03-14 15:43:29 +0100

    Designat index på mibil

---
+++ site/js/main.js
@@ -0,0 +1,7 @@
+function menu(event){
+    let menu = document.querySelector(".menu-box")
+    menu.classList.toggle("activate")
+
+    let bort = document.querySelector(".bort-med-meny")
+    bort.classList.toggle("activate")
+}
