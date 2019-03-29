6124d0d0b8c3118d49381f7575153d89acb57a6c
itggot-zackarias-sabirsh <zackarias.sabirsh@elev.ga.ntig.se>
2019-03-29 23:17:54 +0100

    Desktop klar

--- site/js/main.js
+++ site/js/main.js
@@ -1,16 +1,9 @@
 function menuIndex(event){
-    let menu = document.querySelector(".menu-box")
-    menu.classList.toggle("activate-index")
-
-    let text = document.querySelector(".slogan")
-    text.classList.toggle("activate-slogan")
-
+    let bg = document.querySelector(".bg") 
+    bg.classList.toggle("activate-aside")   
 }
 
 function menuShop(event){
-    let menu = document.querySelector(".menu-box-shop")
-    menu.classList.toggle("activate")
-    
     let aside = document.querySelector("aside")
     aside.classList.toggle("activate-aside")
 }
